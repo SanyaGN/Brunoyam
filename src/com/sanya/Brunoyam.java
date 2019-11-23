a34a23bf5bde688849c73e008f5d17a21433cfb7
Sanya Grigoryan <sgrigoryan@prolevtech.com>
2019-11-23 16:25:02 +0300

    second commit

--- src/com/sanya/Brunoyam.java
+++ src/com/sanya/Brunoyam.java
@@ -2,6 +2,6 @@ package com.sanya;
 
 public class Brunoyam {
     public static void main(String[] args) {
-        System.out.println("Hi Babe");
+        System.out.println("Hi My Babe");
     }
 }
